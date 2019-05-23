0e6495b8ca6a727196f04521d7815cdfd947f24c
xieguocheng <1078700658@qq.com>
2019-05-23 23:35:40 +0800

    第二次更改

--- src/main/java/com/learn/LearnApplication.java
+++ src/main/java/com/learn/LearnApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
     }
